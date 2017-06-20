f3b61c2af63a3c27b479f39a6594afe1247dec81
Doug Hall <wredgum@gmail.com>
2017-06-20 22:59:41 +0100

    pared down code

--- README.md
+++ README.md
@@ -2,17 +2,21 @@
 
 A simple weather display using weather and forecast icons to display on an Waveshare epaper 4.3" display. It will work on Micropython, and may work on CPython implementations if you use another library. This uses the eInk library from https://github.com/dhallgb/eInk-micropython. Icons are taken from The Noun Project (https://thenounproject.com/), and I have used the weather icons from artist 'Per'. I have implemented the Weather Underground and Open Weather Map services, pull offers to add others gratefully received.
 
+## Requirements
+* NodeRED running on server
+* microprocessor running MicroPython
+* NodeRED nodes node-red-contrib-config, or alternate way of setting flow context variables (you could use a function node, for example)
+
 ## Components
-* server component running on NodeRED on server such as Raspberry Pi or Odroid
+* NodeRED flow publishing to MQTT topic
 * embedded microprocessor driving Waveshare e-ink display, such as the ESP8266 or WiPy2
 
 ## Installation
-1. Create accounts on the weather service you wish to use, and generate API keys.
+1. create accounts on the weather service you wish to use, and generate API keys
 1. create the file __config.json__ from the template __config.json.template__ and replace the example values for your selected weather service. 
-1. put the config.sys into your NodeRED data directory
-1. put the config.sys file onto your embedded device
-1. 
+1. install the __weather-config.html/js__ files into your NodeRED node directory
+1. install the e-ink library, and device.py code on your device
 
 ## Usage
-1. run the server component on your host platform on a regular basis, perhaps using either NodeRED or cron jobs
-
+1. trigger the NodeRED flow using whatever method you wish, perhaps using the *inject* node on interval
+1. run the device code, it will loop and sleep 
--- device.py
+++ device.py
@@ -8,59 +8,42 @@ from umqtt.simple import MQTTClient
 from weathericons import interpret_icons
 import gc
 import time
-CONFIG = 'config.json'
-service = 'weatherunderground'
 Tx='G12'
 Rx='G13'
+server='lonna'
+service='wunderground'
 uartnum=1
 
-
-def mungWeather(url,text,stop="."):
-    index = url.find(text)
-    index += len(text)
-    index2 = url.find(stop, index)
-    return(url[index:index2])
-
 def setup():
-    global cfg
-    f = open(CONFIG, 'r')
-    x = f.readall()
-    cfg = ujson.loads(x)
     eink_init()
     eink_clear()
-
-def draw_structure():
     eink_draw_line(300,0,300,599)
     eink_draw_line(300,200,799,200)
     eink_draw_line(300,400,799,400)
     eink_update()
 
 def weather_msg(topic, msg):
-    print((topic, msg))
+    v=interpret_icons(service,str(msg[5]))
+    print(msg)
+    eink_set_en_font(ASCII32)
+    eink_disp_string(v["label"], 50, 250)
+    eink_disp_bitmap(v["icon"]+'.BMP', 100, 100)
+    eink_set_en_font(ASCII64)
+    eink_disp_string(str(w[0]), 100, 350)
+    for i in range(1,4):
+        y = ((i*2)-1)*100
+        eink_disp_string(w[6][i]["low"],400,y)
+        eink_disp_string(w[6][i]["high"],600,y)
+    eink_update()
 
-def main(server="lonna"):
+def main():
     setup()
-    draw_structure()
     c = MQTTClient("eink_display", server)
     c.set_callback(weather_msg)
     c.connect()
     c.subscribe(b"raw/weather")
     while True:
         gc.collect()
-        w=get_weather(service)
-        v=interpret_icons(service,str(w[5]))
-        print(w)
-        eink_set_en_font(ASCII32)
-        eink_disp_string(v["label"], 50, 250)
-        eink_disp_bitmap(v["icon"]+'.BMP', 100, 100)
-        eink_set_en_font(ASCII64)
-        eink_disp_string(str(w[0]), 100, 350)
-        for i in range(1,4):
-            y = ((i*2)-1)*100
-            eink_disp_string(w[6][i]["low"],400,y)
-            eink_disp_string(w[6][i]["high"],600,y)
-        eink_update()
-        sleep(55)
         if True:
             c.wait_msg()
         else:
